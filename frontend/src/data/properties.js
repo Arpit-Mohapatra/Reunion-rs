0d21f170390a69c5e72304d4c249551e618be83c
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2023-08-10 02:06:49 +0530

    added testing dummy data

---
+++ frontend/src/data/properties.js
@@ -0,0 +1,29 @@
+const properties = [
+  {
+    id: 1,
+    title: "Modern Downtown Apartment",
+    type: "Apartment",
+    bedrooms: 2,
+    bathrooms: 2,
+    rent: 1800,
+    imageUrl: "https://example.com/apartment-image.jpg",
+    location: "Downtown, Cityville",
+    amenities: ["Swimming Pool", "Gym", "Parking"],
+    contact: "john.doe@example.com",
+  },
+  {
+    id: 2,
+    title: "Spacious Family House",
+    type: "House",
+    bedrooms: 4,
+    bathrooms: 3,
+    rent: 2500,
+    imageUrl: "https://example.com/house-image.jpg",
+    location: "Suburbia, Cityville",
+    amenities: ["Backyard", "Garage", "Fireplace"],
+    contact: "jane.smith@example.com",
+  },
+  // Add more property entries here
+];
+
+export default properties;
