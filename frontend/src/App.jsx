ab6704bbd186d25dc88a0964558d7a765d6d6ef0
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2023-08-10 11:12:34 +0530

    minor change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { Container } from '@mui/material'
 import './App.css'
 import PropertyApp from './PropertyApp'
 import Header from './components/Header'
-import Footer from './components/Footer'
 
 function App() {
   return (
     <Container>
       <Header></Header>
       <PropertyApp></PropertyApp>
-      <Footer></Footer>
     </Container>
   )
 }
